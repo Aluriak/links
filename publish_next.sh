48e5b36b46683205abe538e2eaa904875c580e1d
lucas bourneuf <lucas.bourneuf@laposte.net>
2017-11-18 18:25:39 +0100

    bugfix + randomly choose the new link

--- publish_next.sh
+++ publish_next.sh
@@ -1,7 +1,7 @@
 source venv/bin/activate
-python3 publish_next.py
+python3 publish_next.py --method=random
 git add content/articles
 curdate=$(date)
-git commit "new link ($curdate)"
+git commit -m "new link ($curdate)"
 make publish
 echo OK
