8e62d9f50a6002ed3680c7c7da5aa2eacba5d103
Aluriak <lucas.bourneuf@laposte.net>
2018-09-18 01:29:49 +0200

    use pietags plugin + yield previously generated pie chart
    small problem about this: pie chart is generated AFTER the page
    generation.
    It would be SO SIMPLER to have a way to include directly HTML and other
    resources, as in https://github.com/getpelican/pelican/pull/1909
    But no, instead, hacking and double compilation is needed…

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,6 +41,7 @@ for page_template in glob('pages_template/*.mkd'):
         ofd.write(ifd.read().format(
             remaining_links=REMAINING_LINKS,
             published_links=PUBLISHED_LINKS,
+            pietags=open('data/pietags.html').read()
         ))
     print('Page {} generated.'.format(page_name))
 
@@ -99,6 +100,7 @@ PLUGINS = [
     'sitemap',  # cf SITEMAP var
 
     # My plugins
+    'pietags',
 ]
 
 
